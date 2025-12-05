445eb24453f5706562e8566c5326339a3840f85c
CodeWithGeorg <gkiarie816@gmail.com>
2025-12-05 13:56:31 +0300

    configured vite.env

--- types.ts
+++ types.ts
@@ -48,3 +48,4 @@ export interface OrderStats {
   name: string;
   value: number;
 }
+
---
+++ vite-env.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="vite/client" />
+
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APPWRITE_ENDPOINT: string;
+  readonly VITE_APPWRITE_PROJECT_ID: string;
+  readonly VITE_APPWRITE_DATABASE_ID: string;
+  readonly VITE_APPWRITE_USERS_COLLECTION_ID: string;
+  readonly VITE_APPWRITE_ORDERS_COLLECTION_ID: string;
+  readonly VITE_APPWRITE_SUBMISSIONS_COLLECTION_ID: string;
+  readonly VITE_APPWRITE_BUCKET_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}

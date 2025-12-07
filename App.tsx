629ec535ce964bcceeee14dec91d1d2f1a9c03f8
CodeWithGeorg <gkiarie816@gmail.com>
2025-12-07 23:54:42 +0300

    updated app

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import Navbar from './components/Navbar';
+import MainLayout from './layouts/MainLayout';
 import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -17,29 +17,29 @@ const App: React.FC = () => {
   return (
     <AuthProvider>
       <HashRouter>
-        <div className="min-h-screen bg-gray-50 font-sans">
-          <Navbar />
-          <main>
-            <Routes>
-              {/* Public Routes */}
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
+        <Routes>
+          {/* Wrap all routes in the MainLayout */}
+          <Route element={<MainLayout />}>
+            
+            {/* Public Routes */}
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
 
-              {/* Client & Admin Shared Routes */}
-              <Route element={<PrivateRoute allowedRoles={[UserRole.CLIENT, UserRole.ADMIN]} />}>
-                <Route path="/dashboard" element={<ClientDashboard />} />
-                <Route path="/contact" element={<Contact />} />
-              </Route>
+            {/* Client & Admin Shared Routes */}
+            <Route element={<PrivateRoute allowedRoles={[UserRole.CLIENT, UserRole.ADMIN]} />}>
+              <Route path="/dashboard" element={<ClientDashboard />} />
+              <Route path="/contact" element={<Contact />} />
+            </Route>
 
-              {/* Admin Routes */}
-              <Route element={<PrivateRoute allowedRoles={[UserRole.ADMIN]} />}>
-                <Route path="/admin" element={<AdminDashboard />} />
-                <Route path="/place-order" element={<PlaceOrder />} />
-              </Route>
-            </Routes>
-          </main>
-        </div>
+            {/* Admin Routes */}
+            <Route element={<PrivateRoute allowedRoles={[UserRole.ADMIN]} />}>
+              <Route path="/admin" element={<AdminDashboard />} />
+              <Route path="/place-order" element={<PlaceOrder />} />
+            </Route>
+            
+          </Route>
+        </Routes>
       </HashRouter>
     </AuthProvider>
   );
